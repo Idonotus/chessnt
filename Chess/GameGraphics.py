267c64b7eae19a0763c6df3b593b31f504a2d67d
Idonotus <reubinius@gmail.com>
2023-05-15 15:41:19 +0100

    refack tor

---
+++ Chess/GameGraphics.py
@@ -0,0 +1,173 @@
+from .vectormath import *
+from .Graphics import PieceSprites
+import warnings
+import tkinter as tk
+class Gui(tk.Canvas):
+    """Handles chess GUI/user interaction"""
+    def __init__(self, master, width=8,height=8,tile=50,signal=None) -> None:
+        self.WIDTH=width
+        self.HEIGHT=height
+        self.TILESIZE=tile
+        self.signal=signal
+        self.highlights={"move":("#FFA3FD","#E384FF"),
+                        "default":("#fcf803","#fcb603"),
+                        "danger":("#E76161","#B04759"),
+                        "normal":("#FFFFFF","#000000"),}
+        super().__init__(master,height=(height)*tile,width=(width)*tile)
+        self.bind("<Button-1>",self.drag_start)
+        self.bind("<B1-Motion>",self.drag_motion)
+        self.bind("<ButtonRelease-1>",self.drag_stop)
+        self.data=[]
+        self.teamcolors=["#FF0000","#0000FF"]
+        self.tiles=[]
+        self.game=None
+        self.ROTATION=0
+        self.draggable=None 
+        
+        self.PIECES = PieceSprites.getallpieces()
+        for x in range(width):
+            self.data.append([])
+            for _ in range(height):
+                self.data[x].append(None)
+        for x in range(0,self.WIDTH):
+            self.tiles.append([])
+            for y in range(0,self.HEIGHT):
+                px,py=self.localrotate(self.ROTATION,x,y)
+                scaledx=px*self.TILESIZE
+                scaledy=py*self.TILESIZE
+                colour=self.highlights["normal"][(x**2+y)%len(self.highlights["normal"])]
+
+
+                rect=self.create_rectangle(scaledx,scaledy,scaledx+tile,scaledy+tile,fill=colour,outline="")
+                self.tag_lower(rect)
+                self.tiles[x].append(rect)
+    def addscheme(self,name,colors:tuple):
+        self.highlights[name]=colors
+
+    def addpiece(self,name,x,y,team):
+        if name not in self.PIECES:
+            warnings.warn(f"Sprite not found for piece \"{name}\". Using dummy instead")
+            name="dummy"
+        if not 0<=x<len(self.data):
+            warnings.warn("Out of bounds")
+            return
+        if not 0<=y<len(self.data[x]):
+            warnings.warn("Out of bounds")
+            return
+        self.data[x][y]=self.PIECES[name](self,self.TILESIZE,x=x,y=y,team=team)
+    
+    def settile(self,x,y,name=None,team=None):
+        if self.data[x][y]:
+                self.data[x][y].delete()
+        if name is None:
+            return
+        if team is None:
+            return
+        self.addpiece(name,x,y,team)
+
+    def localrotate(self,rotation,x,y,inv=False):
+        key=vector(1,1)
+        v=vector(x,y)
+        if inv:
+            rotation=4-rotation
+        key=key.rot90(rotation)
+        v=v.rot90(rotation)
+        x=int(v.x)
+        y=int(v.y)
+        if x<=0 and key.x<0:
+            x=((self.WIDTH-1))+x
+        if y<=0 and key.y<0:
+            y=((self.HEIGHT-1))+y
+        return x,y
+
+
+
+    def removehighlight(self,highlight=None,tiles=[]):
+        atiles=[]+tiles
+        if highlight in self.highlights:
+            atiles+=[vector(self.coords(obj)[0],self.coords(obj)[1])/self.TILESIZE for obj in self.find_withtag(highlight)]
+        self.highlighttiles(atiles,highlight="normal")
+
+    def highlighttiles(self,tiles,*,color:list=[],highlight=""):
+        if highlight not in self.highlights:
+            if color is None or color == []:
+                color=self.highlights["default"]
+        else:
+            color=self.highlights[highlight]
+        for pos in tiles:
+            if isinstance(pos,vector):
+                fill=color[int(pos.x+pos.y)%len(color)]
+                self.itemconfigure(self.tiles[int(pos.x)][int(pos.y)],fill=fill,tags=(highlight,))
+    
+    def applychanges(self,changes):
+        for item in changes:
+            if item[1]:
+                self.settile(item[0][0],item[0][1],item[1].sname,item[1].team)
+            else:
+                self.settile(item[0][0],item[0][1],None)
+
+    def end(self,team):
+        p=tk.Toplevel()
+        tk.Label(p,text=f"Team {team+1} lost").pack()
+
+    def placepiece(self,image,x,y,Piece):
+        x,y=self.localrotate(self.ROTATION,x,y)
+        xpos=x*self.TILESIZE
+        ypos=y*self.TILESIZE
+        
+        xpos-=self.coords(image)[0]
+        ypos-=self.coords(image)[1]
+        if hasattr(Piece,"offset"):
+            xpos+=Piece.offset[0]
+            ypos+=Piece.offset[1]
+        self.move(Piece.image,xpos,ypos)
+    
+    def drag_stop(self,event):
+        widget = self.draggable
+        size=self.TILESIZE
+        if not self.draggable:
+            return
+        x = (self.coords(widget.image)[0]+size//2)
+        y = (self.coords(widget.image)[1]+size//2)
+        
+        if hasattr(widget,"offset"):
+            x-=widget.offset[0]
+            y-=widget.offset[1]
+        x/=size
+        y/=size
+        y=int(y)
+        x=int(x)
+        x,y=self.localrotate(self.ROTATION,x,y,True)
+        self.draggable.returnpiece()
+        self.draggable=None         
+        self.removehighlight(highlight="move")
+        pos2=vector(x,y)
+        if self.signal:
+            self.signal("drop_piece",widget.position,pos2)
+        
+    def drag_start(self,event):
+        tilex=event.x//self.TILESIZE
+        tiley=event.y//self.TILESIZE
+        tilex,tiley=self.localrotate(self.ROTATION,tilex,tiley,True)
+        if 0<=tilex<self.WIDTH and 0<=tiley<self.HEIGHT:
+            if not self.data[tilex][tiley]:
+                return
+            self.draggable=self.data[tilex][tiley]
+            
+            self.tag_raise(self.draggable.image)
+            if self.signal:
+                self.signal("pickup_piece",tilex,tiley)
+
+    def drag_motion(self,event):
+        if not self.draggable:
+            return
+        widget = self.draggable
+        x = event.x - self.TILESIZE//2
+        y = event.y - self.TILESIZE//2
+        image=widget.image
+        xpos=x-self.coords(image)[0]
+        ypos=y-self.coords(image)[1]
+        if hasattr(widget,"offset"):
+            xpos+=widget.offset[0]
+            ypos+=widget.offset[1]
+        self.move(image,xpos,ypos)
---
+++ Chess/GameLogic.py
@@ -0,0 +1,162 @@
+from .Logic import PieceLogic
+import logging
+class Team:
+    """Container for team data"""
+    def __init__(self) -> None:
+        self.kings=[]
+        self.checked=False
+        
+class Logic:
+    """Handles chess logic. Essential for chess to actually work"""
+    def __init__(self,width,height,numteams,game) -> None:
+        self.WIDTH=width
+        self.HEIGHT=height
+        self.data=[]
+        self.teams=[]
+        self.inactiveteams=[]
+        self.teamturn=[1]
+        for i in range(numteams):
+            self.teams.append(Team())
+        for x in range(width):
+            self.data.append([])
+            for _ in range(height):
+                self.data[x].append(None)
+        self.PIECES=PieceLogic.getallpieces()
+        self.game=game
+    
+    def logicexport(self):
+        return {
+            "teams":self.teams,
+            "board":self.data,
+            "dimensions":(self.WIDTH,self.HEIGHT)
+        }
+    
+    def setinactive(self,teamid):
+        if teamid in self.inactiveteams:
+            return
+        self.inactiveteams.append(teamid)
+        p=self.getpieces(teams=[teamid],inactive=False)
+        for t in p:
+            t.erasemoves()
+            t.inactive=True
+
+    def setactive(self,teamid):
+        if teamid not in self.inactiveteams:
+            return
+        self.inactiveteams.remove(teamid)
+        p=self.getpieces(teams=[teamid],inactive=False)
+        for t in p:
+            t.updateMoves()
+            t.inactive=False
+
+    def getpieces(self,data=None,teams=[],teaminv=False,inactive=True):
+        if not data:
+            data=self.data
+        pieces=[]
+        for ystrip in data:
+            for tile in ystrip:
+                if not tile:
+                    continue
+                if not(teaminv ^ (tile.team in teams)):
+                    continue
+                if inactive and hasattr(tile,"inactive"):
+                    if tile.inactive:
+                        continue
+                pieces.append(tile)
+        return pieces
+
+    def endcheck(self):
+        for i,_ in enumerate(self.teams):
+            if hasattr(self.teams[i],"non-player"):
+                continue
+            teammoves=self.getallmoves(teams=[i],cc=True)[2]
+            if len(teammoves)==0:
+                if self.game!=None:
+                    self.game.teamlose(i)
+    
+    def matecheck(self,team):
+        return len(self.getallmoves(teams=[team]))>0
+
+    def makecopy(self):
+        copy=[]
+        for n,ystrip in enumerate(self.data):
+            copy.append([])
+            for tile in ystrip:
+                copy[n].append(tile)
+        return copy
+
+    def validatemove(self,x,y):
+        if not 0<=x<self.WIDTH:
+            return False
+        if not 0<=y<self.HEIGHT:
+            return False
+        return True
+
+    def updateallmoves(self,data=None):
+        if not data:
+            data=self.data
+        p=self.getpieces(data,teaminv=True)
+        for tile in p:
+            tile.updateMoves(data,cc=True)
+
+    def getallmoves(self,data=None,teams=[],teaminv=False,cc=False):
+        if not data:
+            data=self.data
+        p=self.getpieces(data,teams,teaminv)
+        takes=[]
+        moves=[]
+        for piece in p:
+            a,b=piece.getavailmoves(data,cc)
+            moves+=a
+            takes+=b
+        actions=moves+takes
+        return moves,takes,actions
+
+    def genboard():
+        pass
+
+    def addpiece(self,x,y,name,team,**kwargs):
+        name=name.lower()
+        if name not in self.PIECES:
+            logging.warn(f"Logic not found for piece \"{name}\". Using dummy instead")
+            name="dummy"
+        if not 0<=x<len(self.data):
+            logging.warn("Out of bounds")
+        if not 0<=y<len(self.data[x]):
+            logging.warn("Out of bounds")
+        self.data[x][y]=self.PIECES[name](logic=self,x=x,y=y,team=team,**kwargs)
+
+    def getdifferences(self,base:list,changed:list):
+        diff=[((x,y),changed[x][y]) for x in range(len(changed)) for y in range(len(changed[x])) if changed[x][y]!=base[x][y]]
+        return diff
+
+    def canmove(self,pos1,pos2,team=None):
+        if not self.validatemove(pos2.x,pos2.y):
+            return False
+        if team is not None and team not in self.teamturn:
+                return False
+        movepiece=self.getpiece(pos=pos1)
+        if not movepiece:
+            return False
+        if movepiece.team!=team and team is not None:
+            return False
+        return movepiece.canmove(pos2)
+
+    def movepiece(self,pos1,pos2):
+        p=self.getpiece(pos=pos1)
+        if not p:
+            return
+        c=self.makecopy()
+        a=p.move(pos2,self.data)
+        if a=="return":
+            return
+        return self.getdifferences(c,self.data)
+        
+    def ispiece(self,*, pos=None, x=None, y=None):
+        return bool(self.getpiece(pos=pos,x=x,y=y))
+
+    def getpiece(self,*, pos=None, x=None, y=None):
+        if pos:
+            return self.data[int(pos.x)][int(pos.y)]
+        if not (x is None and y is None):
+            return self.data[x][y]
