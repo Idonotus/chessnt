fd94056710b57af3310a3d9910d5a4ef522424be
Idouse <119412556+Idouse@users.noreply.github.com>
2023-05-16 15:41:08 +0100

    aaaaaaaaaa

--- Chess/GameLogic.py
+++ Chess/GameLogic.py
@@ -1,11 +1,12 @@
 from .Logic import PieceLogic
+from .Logic.Piece import Piece
 import logging
 class Team:
     """Container for team data"""
     def __init__(self) -> None:
         self.kings=[]
         self.checked=False
-        
+
 class Logic:
     """Handles chess logic. Essential for chess to actually work"""
     def __init__(self,width,height,numteams,game) -> None:
@@ -147,6 +148,15 @@ class Logic:
         
     def ispiece(self,*, pos=None, x=None, y=None):
         return bool(self.getpiece(pos=pos,x=x,y=y))
+
+    def convertgui(self,var):
+        if isinstance(var,Piece):
+            return var.GuiExport()
+        if isinstance(var,list):
+            temp=[]
+            for item in var:
+                temp.append(self.convertgui(item))
+            return temp
 
     def getpiece(self,*, pos=None, x=None, y=None):
         if pos:
